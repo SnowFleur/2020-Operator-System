c4f938685c9f60d62bd2e8be3266ea4e32a092f1
SnowFleur <SnowFleur0128@gmail.com>
2020-10-03 01:51:03 +0900

    Add SpinLock Code, Test Code

--- 병행성(Parallelism)/Spin-Lock/SpinLock.cpp
+++ 병행성(Parallelism)/Spin-Lock/SpinLock.cpp
@@ -3,18 +3,20 @@
 
 
 
-bool CSpinLock::CAS(std::atomic_int* addr, int  expected, int new_val){
-    return std::atomic_compare_exchange_strong(addr, &expected, new_val);
+bool CSpinLock::CAS( int*  addr, int  expected, int new_val){
+    return std::atomic_compare_exchange_strong(
+        reinterpret_cast<std::atomic_int*>(addr), &expected, new_val);
 }
 
-void CSpinLock::SpinLock() {
-    while (CAS(&flag, 0, 1) == 1);
+void CSpinLock::SpinLock()noexcept {
+    while (lock.test_and_set(std::memory_order_acquire));
+    //while (!CAS(&casValue_, 0, 1));
 }
 
-void CSpinLock::SpinUnLock() {
-    CAS(&flag, 1, 0);
+void CSpinLock::SpinUnLock()noexcept {
+    lock.clear(std::memory_order_release);
+    //CAS(&casValue_, 1, 0);
 }
-
 
 void CSpinLock::SumByLock() {
     for (int i = 0; i < MAX_LOOP; ++i) {
@@ -34,26 +36,29 @@ void CSpinLock::SumBySpinLock() {
 
 void CSpinLock::Run() {
 
-  
-
-    //Spin Lock
-  /*  {
-        for (int i = 0; i < MAX_THREADS; ++i) {
-            std::thread thread(&CSpinLock::SumBySpinLock, this);
-        }
+    std::cout << "====Number Theads: " << MAX_THREADS << "=====\n\n\n";
+#pragma region Spin Lock
+    {
+        Threads     threads_;
         auto startTime = high_resolution_clock::now();
-        auto EndTime = high_resolution_clock::now();
+        for (int i = 0; i < MAX_THREADS; ++i) {
+            threads_.emplace_back(std::thread(&CSpinLock::SumBySpinLock, this));
+        }
+        for (auto& thread : threads_)
+            thread.join();
+        auto EndTime = high_resolution_clock::now(); 
+
         auto elapsedTime = EndTime - startTime;
         std::cout << "Sum Result: " << g_sum << "\n";
-            std::cout << "Elapsed Time For Lock: " << 
-            duration_cast<std::chrono::milliseconds>(elapsedTime).count() << "sec\n";
-    }*/
+        std::cout << "Elapsed Time For Spin Lock: " <<
+            duration_cast<std::chrono::milliseconds>(elapsedTime).count() << "millsec\n\n";
+    }
+#pragma endregion 
 
-
-    //Lock
+#pragma region Mutex Lock
     {
+        Threads     threads_;
         g_sum = 0;
-    
         auto startTime = high_resolution_clock::now();
         for (int i = 0; i < MAX_THREADS; ++i) {
             threads_.emplace_back(std::thread(&CSpinLock::SumByLock, this));
@@ -65,8 +70,7 @@ void CSpinLock::Run() {
         auto elapsedTime = EndTime - startTime;
         std::cout << "Sum Result: " << g_sum << "\n";
         std::cout << "Elapsed Time For Lock: " << 
-            duration_cast<std::chrono::seconds>(elapsedTime).count() << "sec\n";
+            duration_cast<std::chrono::milliseconds>(elapsedTime).count() << "millsec\n";
     }
-
-
+#pragma endregion
 }
--- 병행성(Parallelism)/Spin-Lock/SpinLock.h
+++ 병행성(Parallelism)/Spin-Lock/SpinLock.h
@@ -6,20 +6,23 @@
 #include<thread>
 
 
-constexpr int MAX_THREADS   = 6;
-constexpr int MAX_LOOP      = 50000000;
+constexpr int MAX_THREADS   = 4;
+constexpr int MAX_LOOP      = 10000000;
 
 using namespace std::chrono;
-using Threads = std::vector<std::thread>;
-using mutex = std::mutex;
+using Threads   = std::vector<std::thread>;
+using mutex     = std::mutex; 
+
 class CSpinLock{
 private:
-    mutex       mutex_;
-    Threads     threads_;
-    void        SpinLock();
-    void        SpinUnLock();
-    bool        CAS(std::atomic_int* addr, int  expected, int new_val);
-    
+    mutex           mutex_;
+    int             casValue_=0;
+    volatile int    g_sum=0;
+    std::atomic_flag lock = ATOMIC_FLAG_INIT;
+
+    void        SpinLock()noexcept;
+    void        SpinUnLock()noexcept;
+    bool        CAS( int*  addr, int  expected, int new_val);
     void        SumBySpinLock();
     void        SumByLock();
 public:
@@ -28,6 +31,3 @@ public:
     
     void        Run();
 };
-
-static volatile int g_sum{ 0 };
-static std::atomic_int flag{};
